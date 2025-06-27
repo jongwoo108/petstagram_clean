fa5d07a54118b906c81acf6e0fadb1030e4853f9
Jongwoo Shin <jongwoo108@gmail.com>
2025-06-28 00:31:19 +0900

    dockerized

--- petstagram_be/main.py
+++ petstagram_be/main.py
@@ -8,7 +8,11 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173",
+                   "http://127.0.0.1:5173",
+                   "http://localhost:3000",
+                   "http://127.0.0.1:3000",
+                   "http://fastapi-backend:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
